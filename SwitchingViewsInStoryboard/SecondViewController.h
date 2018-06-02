991e7d1897db6835d629b1f2b03b3e92bbdd5b67
Juhnowski <juhnowski@gmail.com>
2018-06-02 22:05:17 +0300

    first commit

---
+++ SwitchingViewsInStoryboard/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  SwitchingViewsInStoryboard
+//
+//  Created by Илья Юхновский on 02.06.2018.
+//  Copyright © 2018 Илья Юхновский. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+- (IBAction)dismissView:(id)sender;
+- (IBAction)dismissButton:(id)sender;
+- (IBAction)dismiss:(id)sender;
+
+@end
